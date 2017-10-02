1ebf9e926e8f6dffe689e18a39b725455ea46836
Nikita Shavrin <neketsh4@gmail.com>
2017-10-02 21:19:49 +0300

    - base commit

---
+++ Projects/RecepiesProject/app/src/main/java/com/receiptsproject/CategoriesFragment.java
@@ -0,0 +1,10 @@
+package com.receiptsproject;
+
+import android.app.Fragment;
+
+public class CategoriesFragment extends Fragment {
+    //todo create layout
+    //todo adding new categories
+    //todo deleting categories
+    //todo add transition on ReceiptsFragment
+}
---
+++ Projects/RecepiesProject/app/src/main/java/com/receiptsproject/ReceiptsFragment.java
@@ -0,0 +1,9 @@
+package com.receiptsproject;
+
+import android.app.Fragment;
+
+public class ReceiptsFragment extends Fragment {
+    //todo create layout
+    //todo adding new receipts
+    //todo deleting receipts
+}
