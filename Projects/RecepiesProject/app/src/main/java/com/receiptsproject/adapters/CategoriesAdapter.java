f974d84145c0fed71a2ce9ae04126367ae307130
Nikita Shavrin <neketsh4@gmail.com>
2017-10-03 09:48:09 +0300

    - add info in blanks for adapters

--- Projects/RecepiesProject/app/src/main/java/com/receiptsproject/adapters/CategoriesAdapter.java
+++ Projects/RecepiesProject/app/src/main/java/com/receiptsproject/adapters/CategoriesAdapter.java
@@ -1,5 +1,29 @@
 package com.receiptsproject.adapters;
 
-public class CategoriesAdapter {
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
 
+public class CategoriesAdapter extends RecyclerView.Adapter<CategoriesAdapter.MyAdapter>{
+
+    @Override
+    public CategoriesAdapter.MyAdapter onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(CategoriesAdapter.MyAdapter holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class MyAdapter extends RecyclerView.ViewHolder {
+        public MyAdapter(View itemView) {
+            super(itemView);
+        }
+    }
 }
--- Projects/RecepiesProject/app/src/main/java/com/receiptsproject/adapters/ReceiptsAdapter.java
+++ Projects/RecepiesProject/app/src/main/java/com/receiptsproject/adapters/ReceiptsAdapter.java
@@ -1,5 +1,30 @@
 package com.receiptsproject.adapters;
 
 
-public class ReceiptsAdapter {
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ReceiptsAdapter extends RecyclerView.Adapter<ReceiptsAdapter.MyAdapter>{
+
+    @Override
+    public ReceiptsAdapter.MyAdapter onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(ReceiptsAdapter.MyAdapter holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class MyAdapter extends RecyclerView.ViewHolder {
+        public MyAdapter(View itemView) {
+            super(itemView);
+        }
+    }
 }
