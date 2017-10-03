063eeb6007099b944588c672ae9eef4db012ce7e
GrossGhost <grossghost13@gmail.com>
2017-10-03 11:15:09 +0300

    created layout for CategoriesFragment

--- Projects/RecepiesProject/app/src/main/java/com/receiptsproject/fragments/CategoriesFragment.java
+++ Projects/RecepiesProject/app/src/main/java/com/receiptsproject/fragments/CategoriesFragment.java
@@ -1,10 +1,31 @@
 package com.receiptsproject.fragments;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.receiptsproject.R;
 
 public class CategoriesFragment extends Fragment {
     //todo create layout
     //todo adding new categories
     //todo deleting categories
     //todo add transition on ReceiptsFragment
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.categories_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        FloatingActionButton fab = view.findViewById(R.id.categories_fab);
+    }
 }
